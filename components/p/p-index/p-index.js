b4d4b6c3510f4a8391c7802bf4a83d96af557f32
koala <dapixp@gmail.com>
2015-07-07 01:56:12 +0800

    页面调整

--- components/p/p-index/p-index.js
+++ components/p/p-index/p-index.js
@@ -15,21 +15,24 @@ Vue.component('p-index', {
                 name: '小云精选',
                 url: '/',
                 image: __uri('images/0.png'),
-                desc: '订阅内容'
+                desc: '订阅内容',
+                isActive: false
             }, {
                 id: 14,
                 name: 'Dribbble',
                 url: 'https://dribbble.com',
                 image: __uri('images/14.png'),
-                desc: 'Dribbble - 优秀设计师的聚集地'
+                desc: 'Dribbble - 优秀设计师的聚集地',
+                isActive: false
             }, {
                 id: 34,
                 name: 'UI中国',
                 url: 'http://www.ui.cn/',
                 image: __uri('images/34.png'),
-                desc: 'UI中国 - 国内最专业的UI设计平台'
+                desc: 'UI中国 - 国内最专业的UI设计平台',
+                isActive: false,
             }],
-            currentRss: null
+            currentRss: {}
         }
     },
     ready() {
@@ -37,20 +40,20 @@ Vue.component('p-index', {
         let rssId;
 
         if (!params || !params.rssId) {
-            this.isIndex = true;
             restApi.recom.get()
                 .success(res => {
+                    let data = res.data;
+                    data.items = this._mapFeed(this._mapHost(data.items));
+
                     this.currentRss = this.rss[0];
-                    this.currentRss.data = res.data;
-                    
+                    this.currentRss.data = data;
+                    this.rss[0].isActive = true;
                     console.log(res.data);
                 })
                 .error(err => {
 
                 });
         } else {
-            this.isIndex = false;
-
             rssId = params.rssId;
 
             restApi.rss.get({id: rssId})
@@ -63,6 +66,62 @@ Vue.component('p-index', {
         }
     },
     methods: {
+        _mapHost(data) {
+            let originalHost = 'http://idesign.qq.com';
+            data.forEach(function(item) {
+                item.thumb = originalHost + item.thumb;
+                item.thumb_src = originalHost + item.thumb_src;
+                item.url = originalHost + item.url;
+            });
+
+            return data;
+        },
+
+        _mapFeed(data) {
+            const t = new Date();
+            const year = t.getFullYear();
+            const month = ("00000" + (t.getMonth() + 1)).substr(("00000" + (t.getMonth() + 1)).length - 2);
+            const today = ("00000" + t.getDate()).substr(("00000" + t.getDate()).length - 2);
+            const yestoday = ("00000" + (t.getDate() - 1)).substr(("00000" + (t.getDate() - 1)).length - 2);
+
+            const tDate = year + "-" + month + "-" + today;
+            const yDate = year + "-" + month + "-" + yestoday;
+            const bDate = year + "-" + month + "-" + (yestoday - 1);
+
+            let todayData = {
+                dataName: '今天',
+                dateTime: tDate,
+                data: []
+            };
+
+            let yestodayData = {
+                dataName: '今天',
+                dateTime: yDate,
+                data: []
+            };
+
+            let beforeData = {
+                dataName: '以前',
+                dateTime: bDate,
+                data: []
+            };
+
+            data.forEach(function(item, index) {
+                let _date;
+                _date = item.create_on && item.create_on.substring(0, 10) ? item.create_on.substring(0, 10) : bDate;
+
+                if (_date === tDate) {
+                    todayData.data.push(item);
+                } else if (_date === yDate) {
+                    yestodayData.data.push(item);
+                } else {
+                    beforeData.data.push(item);
+                }
+            });
+
+            return [todayData, yestodayData, beforeData];
+        },
+
         onTap(item, index) {
             if (item.params) Vue.go(item.type, item.params);
             else Vue.go(item.type);
--- components/p/p-index/p-index.tpl
+++ components/p/p-index/p-index.tpl
@@ -7,7 +7,7 @@
             </div>
             <div class="feed-sidebar-list">
                 <ul>
-                    <li v-repeat="rss">
+                    <li v-repeat="rss" v-class="current: isActive">
                         <span class="feed-logo">
                             <img src="{{image}}" alt="{{id}}" />
                         </span>
@@ -22,9 +22,34 @@
         </div>
         <div class="feed-container">
             <div class="feed-container-header">
-                <h2>{{currentRss.name}}</h2>
+                <h2>{{currentRss.desc}}</h2>
             </div>
             <div class="feed-items-container">
+                <div v-repeat="item: currentRss.data.items" v-show="item.data.length">
+                    <div class="feed-items-timeline">{{item.dataName}}</div>
+                    <div class="feed-item" v-repeat="item.data">
+                        <div class="feed-thumb" style="background-image: url({{thumb}})">
+                            <a href="{{url}}" target="_blank"><img src="{{thumb}}" alt="Flux – Joseph a avoué"></a>
+                        </div>
+                        <div class="feed-describe">
+                            <h2 class="feed-title"><a href="{{url}}" target="_blank">{{title}}</a></h2>
+                            <p class="feed-description">{{description}}</p>
+                            <div class="feed-item-info">
+                                <div class="feed-item-count">
+                                    <span class="feed-fav">
+                                        <i class="g-icon icon-fav favHandler"></i>
+                                        <i class="feed-fav-total">{{faved}}</i>
+                                    </span>
+                                    <span class="feed-view">
+                                        <i class="g-icon icon-viewed"></i>
+                                        <i class="feed-view-total">{{click}}</i>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     </div>
