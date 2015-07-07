dcc632bef55f4f8db1e8a138cb1faeb1b2cab7f6
okoala <dapixp@gmail.com>
2015-07-07 11:55:14 +0800

    fix release

--- build/gulp/tasks/deploy.js
+++ build/gulp/tasks/deploy.js
@@ -8,8 +8,8 @@ var config = require('../../../config');
 gulp.task('webpack', shell.task('webpack --config ../../webpack/webpack.config.js'));
 // start
 // 发布相关
-gulp.task('release_test', ['webpack'], shell.task('fis3 release -Dompd ' + config.dest));
-gulp.task('release_prod', ['webpack'], shell.task('fis3 release -Dompd ' + config.dest));
+gulp.task('release_test', ['webpack'], shell.task('fis3 release test -d ' + config.dest));
+gulp.task('release_prod', ['webpack'], shell.task('fis3 release prod -d ' + config.dest));
 
 gulp.task('deploy_test', ['release_test'], function() {
     shell.task('npm run start_test', {
