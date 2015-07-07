9f442e0821fae082d5d60a7cd35d8c2fdbb4a865
okoala <dapixp@gmail.com>
2015-07-07 14:09:33 +0800

    修复发布

--- build/gulp/tasks/deploy.js
+++ build/gulp/tasks/deploy.js
@@ -8,17 +8,10 @@ var config = require('../../../config');
 gulp.task('webpack', shell.task('webpack --config ../../webpack/webpack.config.js'));
 // start
 // 发布相关
-gulp.task('release_test', ['webpack'], shell.task('fis3 release test -d ' + config.dest));
-gulp.task('release_prod', ['webpack'], shell.task('fis3 release prod -d ' + config.dest));
+gulp.task('release', ['webpack'], shell.task('fis3 release prod -d ' + config.dest));
 
-gulp.task('deploy_test', ['release_test'], function() {
-    shell.task('npm run start_test', {
-        cwd: config.dest
-    })();
-});
-
-gulp.task('deploy_prod', ['release_prod'], function() {
-    shell.task('npm run start_prod', {
+gulp.task('deploy', ['release'], function() {
+    shell.task('npm run start', {
         cwd: config.dest
     });
 });
