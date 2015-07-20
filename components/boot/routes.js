fd0750a5c8c61a6f5438db322af65da2ce25185d
okoala <dapixp@gmail.com>
2015-07-20 17:15:04 +0800

    UI Component

--- components/boot/routes.js
+++ components/boot/routes.js
@@ -1,65 +1,68 @@
-'use strict';
+'use strict'
 
-export default function(Vue) {
+export default function (Vue) {
 
-    let options = {
-        // 设置跟节点
-        el: 'body',
+  return new Vue({
+    // 设置跟节点
+    el: 'body',
 
-        //路由状态
-        router: {
-            states: {
-                'index': {
-                    url: '/',
-                    component: 'p-index'
-                },
-
-                'rss': {
-                    url: '/:rssId',
-                    component: 'p-index'
-                },
-
-                //404找不到页面
-                //404必须放到最后
-                'notFound': {
-                    url: '*',
-                    component: 'p-notFound'
-                }
-            },
-            options: {
-                // 页面模块默认全异步加载。
-                async: true,
-                // 使用html5 mode
-                hashbang: false,
-                // 设置页面间的转场效果
-                transition: true
-            },
-            // 全局中间件
-            middleware: {
-                // 在路由进入前执行
-                // 可以用作获取用户信息，登陆/授权相关~~
-                beforeEnter: function(location, next, router) {
-                    // 这里可以加载restApi
-                    // Vue.$root.isLogin = true;
-                }
-            }
+    // 路由状态
+    router: {
+      states: {
+        'material': {
+          url: '/material',
+          component: 'p-material'
         },
 
-        //设置路由的事件~
-        events: {
-            'router:beforeEnter': function(to, from) {
+        'index': {
+          url: '/',
+          component: 'p-index'
+        },
 
-            },
+        'rss': {
+          url: '/:rssId',
+          component: 'p-index'
+        },
 
-            'router:onEnter': function(to, from) {
-                console.log('true');
-            },
-
-            'router:beforeLeave': function() {
-
-            }
+        // 404找不到页面
+        // 404必须放到最后
+        'notFound': {
+          url: '*',
+          component: 'p-notFound'
         }
-    };
+      },
+      options: {
+        // 页面模块默认全异步加载。
+        async: true,
+        // 使用html5 mode
+        hashbang: false,
+        // 设置页面间的转场效果
+        transition: true
+      },
+      // 全局中间件
+      middleware: {
+        // 在路由进入前执行
+        // 可以用作获取用户信息，登陆/授权相关~~
+        beforeEnter: function (location, next, router) {
+          // 这里可以加载restApi
+          // Vue.$root.isLogin = true;
+        }
+      }
+    },
 
-    new Vue(options);
+    // 设置路由的事件~
+    events: {
+      'router:beforeEnter': function (to, from) {
+
+      },
+
+      'router:onEnter': function (to, from) {
+
+      },
+
+      'router:beforeLeave': function () {
+
+      }
+    }
+  })
 }
---
+++ components/p/p-material/p-material.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import Vue from 'vue'
+
+Vue.component('p-material', {
+  template: __inline('p-material.tpl'),
+  data () {
+    return {}
+  },
+  methods: {}
+})
